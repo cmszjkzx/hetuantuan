3bf9f993e401fc8fbb9d44ef31b025140ddfa0fc
ruyanfly <yanru545@126.com>
2016-12-09 21:32:17 +0800

    优惠券领取功能BUG修复

--- system/shopwap/class/mobile/getbonus.php
+++ system/shopwap/class/mobile/getbonus.php
@@ -9,72 +9,43 @@
 $member=get_member_account();
 $openid =$member['openid'] ;
 $weixin_openid = $member['weixin_openid'];
-if(!empty($_GP['id'])){
-    $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  id = ".$_GP['id']);
-    if(checksubmit('submit')){
-        $phone = $_GP['phone'];
-        if(!empty($phone)){
-            $adv_bonus = array();
-            if(!empty($adv['hasbonus'])){
-                $adv_bonus = explode('-',$adv['hasbonus']);
-                array_shift($adv_bonus);
+if(empty($_GP['op'])){
+    if(empty($_GP['id'])){
+        $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  displayorder = 99");
+    }else{
+        $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  id = " . $_GP['id']);
+        $id = $_GP['id'];
+    }
+    include themePage('getbonus');
+}else if('submit' == $_GP['op']){
+    if(empty($_GP['id'])){
+        $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  displayorder = 99");
+    }else{
+        $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  id = " . $_GP['id']);
+    }
+    $adv_bonus = array();
+    if (!empty($adv['hasbonus'])) {
+        $adv_bonus = explode('-', $adv['hasbonus']);
+        array_shift($adv_bonus);
+    }
+    foreach ($adv_bonus as $bonusid) {
+        if (!empty($openid)) {
+            $user_bonus = mysqld_select("SELECT * FROM " . table('bonus_user') . " WHERE  openid = " . $openid . " AND bonus_type_id = " . $bonusid);
+            if (empty($user_bonus)) {
+                $bonus_sn = date("Ymd", time()) . $rank_model['type_id'] . rand(1000000, 9999999);
+                $data = array('createtime' => time(),
+                    'openid' => $openid,
+                    'weixin_openid' => $weixin_openid,
+                    'bonus_sn' => $bonus_sn,
+                    'deleted' => 0,
+                    'isuse' => 0,
+                    'bonus_type_id' => $bonusid);
+                mysqld_insert('bonus_user', $data);
+            } else {
+                die(json_encode(array('result' => 1)));
             }
-            foreach ($adv_bonus as $bonusid) {
-                if (!empty($openid)) {
-                    $user_bonus = mysqld_select("SELECT * FROM " . table('bonus_user') . " WHERE  openid = ".$openid." AND bonus_type_id = ".$bonusid);
-                    if(empty($user_bonus)){
-                        $bonus_sn = date("Ymd", time()) . $rank_model['type_id'] . rand(1000000, 9999999);
-                        $data = array('createtime' => time(),
-                            'openid' => $openid,
-                            'weixin_openid' => $weixin_openid,
-                            'bonus_sn' => $bonus_sn,
-                            'deleted' => 0,
-                            'isuse' => 0,
-                            'bonus_type_id' => $bonusid);
-                        mysqld_insert('bonus_user', $data);
-                    } else {
-                        echo "<script>tip('您已领取过',true)</script>";
-                    }
-                }
-                header("location: " . mobile_url('bonus'));
-            }
-        }else{
-            message("请输入手机号!");
         }
     }
-}else{
-    $adv = mysqld_select("SELECT * FROM " . table('shop_adv') . " WHERE  displayorder = 99");
-    if(checksubmit('submit')){
-        $phone = $_GP['phone'];
-        if(!empty($phone)){
-            $adv_bonus = array();
-            if(!empty($adv['hasbonus'])){
-                $adv_bonus = explode('-',$adv['hasbonus']);
-                array_shift($adv_bonus);
-            }
-            foreach ($adv_bonus as $bonusid) {
-                if (!empty($openid)) {
-                    $user_bonus = mysqld_select("SELECT * FROM " . table('bonus_user') . " WHERE  openid = ".$openid." AND bonus_type_id = ".$bonusid);
-                    if(empty($user_bonus)){
-                        $bonus_sn = date("Ymd", time()) . $rank_model['type_id'] . rand(1000000, 9999999);
-                        $data = array('createtime' => time(),
-                            'openid' => $openid,
-                            'weixin_openid' => $weixin_openid,
-                            'bonus_sn' => $bonus_sn,
-                            'deleted' => 0,
-                            'isuse' => 0,
-                            'bonus_type_id' => $bonusid);
-                        mysqld_insert('bonus_user', $data);
-                    } else {
-                        echo "<script>tip('您已领取过',true)</script>";
-                    }
-                }
-                header("location: " . mobile_url('bonus'));
-            }
-        }else{
-            message("请输入手机号!");
-        }
-    }
+    header("location: " . mobile_url('bonus'));
+    exit;
 }
-
-include themePage('getbonus');
