c8352cc1caac3d4b69db3a3360e27a03b4c0a199
cmszjkzx <young@youngdeMacBook-Pro.local>
2017-02-07 11:34:17 +0800

    gitignore

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('SYSTEM_IN') or exit('Access Denied');
-$HETUANTUAN_CONFIG = array();
-$HETUANTUAN_CONFIG['db']['host'] = '127.0.0.1';
-$HETUANTUAN_CONFIG['db']['username'] = 'root';
-$HETUANTUAN_CONFIG['db']['password'] = 'yanru545';
-$HETUANTUAN_CONFIG['db']['port'] = '3306';
-$HETUANTUAN_CONFIG['db']['database'] = 'hetuantuan';
