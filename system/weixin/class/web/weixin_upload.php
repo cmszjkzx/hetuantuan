55dbd5bd49f849e4ad2dd11222ebfe4020d1b462
ruyanfly <yanru545@126.com>
2017-02-14 17:39:07 +0800

    微信素材上传BUG修复

--- system/weixin/class/web/weixin_upload.php
+++ system/weixin/class/web/weixin_upload.php
@@ -10,64 +10,62 @@
 $access_token = get_weixin_token();
 //$url = "https://api.weixin.qq.com/cgi-bin/material/batchget_material?access_token={$access_token}";
 //$return = $this->get_all_media($url);
-if(checksubmit()){
+if(checksubmit()) {
     if (!empty($_FILES['material']['tmp_name'])) {
         $upload = material_upload($_FILES['material']);
         if (is_error($upload)) {
             message($upload['message'], '', 'error');
         }
-        $filename = "/attachment/".$upload['path'];
-    }else{
+        $filename = "/attachment/" . $upload['path'];
+    } else {
         message("请上传素材!");
     }
-    if(0 == $_GP['status']){
-        if(empty($_GP['type'])){
+    if (0 == $_GP['status']) {
+        if (empty($_GP['type'])) {
             message("请选择上传的素材类型!");
-        }else{
-            $media = array('filename'=>$filename,
-                'filelength'=>$_FILES['material']['size'],
-                'content-type'=>$_FILES['material']['type']);
-            $data = array('type'=>$_GP['type'],
-                'media'=>$media);
+        } else {
+            $media = array('filename' => $filename,
+                'filelength' => $_FILES['material']['size'],
+                'content-type' => $_FILES['material']['type']);
+            $data = array('type' => $_GP['type'],
+                'media' => $media);
         }
-    }else if(1 == $_GP['status']){
-        if(empty($_GP['title'])){
+    } else if (1 == $_GP['status']) {
+        if (empty($_GP['title'])) {
             message("请输入标题!");
         }
-        if(empty($_GP['thumb_media_id'])){
+        if (empty($_GP['thumb_media_id'])) {
             message("请输入图片素材ID!");
         }
-        if(empty($_GP['author'])){
+        if (empty($_GP['author'])) {
             message("请输入作者名称!");
         }
-        if(!isset($_GP['show_cover_pic'])){
+        if (!isset($_GP['show_cover_pic'])) {
             message("请选择是否显示封面!");
         }
-        if(empty($_GP['content'])){
+        if (empty($_GP['content'])) {
             message("请输入图文具体内容!");
         }
-        if(empty($_GP['content_source_url'])){
+        if (empty($_GP['content_source_url'])) {
             message("请输入原文地址!");
         }
         $_GP['content'] = "this is a test!";
-        $data = array('title'=>$_GP['title'],
-            'thumb_media_id'=>$_GP['thumb_media_id'],
-            'author'=>$_GP['author'],
-            'digest'=>$_GP['digest'],
-            'show_cover_pic'=>$_GP['show_cover_pic'],
-            'content'=>$_GP['content'],
-            'content_source_url'=>$_GP['content_source_url']
+        $data = array('title' => $_GP['title'],
+            'thumb_media_id' => $_GP['thumb_media_id'],
+            'author' => $_GP['author'],
+            'digest' => $_GP['digest'],
+            'show_cover_pic' => $_GP['show_cover_pic'],
+            'content' => $_GP['content'],
+            'content_source_url' => $_GP['content_source_url']
         );
-    }else{
+    } else {
         message("请选择是否为永久素材!");
     }
-    $return= $this->uploadMaterial($data, $_GP['status'],$access_token);
-}
-if($return==true)
-{
-    message('修改成功', 'refresh', 'success');
-}else
-{
-    message($return);
+    $return = $this->uploadMaterial($data, $_GP['status'], $access_token);
+    if ($return == true) {
+        message('修改成功', 'refresh', 'success');
+    } else {
+        message($return);
+    }
 }
 include page('weixin_upload');
