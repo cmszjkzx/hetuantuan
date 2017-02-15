7ec3062ddc140ca33b43ade7efc97cc5ca9dc419
ruyanfly <yanru545@126.com>
2017-02-15 20:52:02 +0800

    团购BUG修复

--- system/public/class/web/installsql.php
+++ system/public/class/web/installsql.php
@@ -1088,7 +1088,7 @@ CREATE TABLE `hetuantuan_group_user` (
   `goodid` varchar(100) NOT NULL DEFAULT '' COMMENT '商品编号',
   `openid` varchar(50) DEFAULT NULL,
   `weixin_openid` varchar(50) DEFAULT NULL,
-  `isgroup` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否参团，0为否，1为是',
+  `ispraise` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否参团，0为否，1为是',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- ----------------------------
--- system/shopwap/class/mobile/bangnicai.php
+++ system/shopwap/class/mobile/bangnicai.php
@@ -8,19 +8,12 @@
 $member=get_member_account();
 $openid =$member['openid'] ;
 $weixin_openid = $member['weixin_openid'];
-
-$isgroup = mysqld_select("SELECT isgroup FROM " . table('group_user') . " WHERE goodid = :goodid AND weixin_openid = :weixin_openid", array(':goodid'=>$_GP['id'], ':weixin_openid'=>$weixin_openid));
-if(!empty($isgroup['isgroup'])) {
-    $status = 1;
-}else if(null != $isgroup['isgroup']) {
-    $status = 0;
-}else{
-    $status = -1;
-}
-
 if (empty($_GP['op']) || 'collection'==$_GP['op']){
     $head_thumb = mysqld_select("SELECT thumb FROM " . table('shop_adv'). " WHERE link=:link ",array(":link"=>'group'));
-    $list = mysqld_selectall("SELECT * FROM " . table('group')." WHERE isshow=:isshow AND isgroup=:isgroup ", array(":isshow"=>1, "isgroup"=>0));
+    $list = mysqld_selectall("SELECT a.id, a.goodname, a.isshow, a.limittime, a.isgroup, a.goodsn, a.praise, a.thumb, a.sucessthumb,
+ a.description, a.express, SUM(a.ispraise) AS ispraise FROM (SELECT hetuantuan_group.*, IF(hetuantuan_group.id=hetuantuan_group_user.goodid,1,0) 
+  AS ispraise from hetuantuan_group, hetuantuan_group_user WHERE hetuantuan_group.isshow=:isshow AND hetuantuan_group.isgroup=:isgroup AND 
+  hetuantuan_group_user.weixin_openid=:weixin_openid) a GROUP BY a.id ", array(":isshow"=>1, "isgroup"=>0, ':weixin_openid'=>$weixin_openid));
     $option = 0;
     include themePage('bangnicai');
 }else if ('success' == $_GP['op']){
@@ -29,24 +22,25 @@ if (empty($_GP['op']) || 'collection'==$_GP['op']){
     $option = 1;
     include themePage('bangnicai');
 }else if ('addpraise' == $_GP['op']){
-    if(1 == $status){
+    $isgroup = mysqld_select("SELECT isgroup FROM " . table('group_user') . " WHERE goodid = :goodid AND weixin_openid = :weixin_openid", array(':goodid'=>$_GP['id'], ':weixin_openid'=>$weixin_openid));
+    if(!empty($isgroup['isgroup'])){
         $item = mysqld_select("SELECT praise FROM " . table('group') . " WHERE id = :id", array(':id'=>$_GP['id']));
         $item['praise']--;
         mysqld_update('group', array('praise' => $item['praise']), array('id'=>$_GP['id']));
-        $status = 0;
+        $isgroup['isgroup'] = 0;
         mysqld_update('group_user', array('isgroup' => 0), array('goodid'=>$_GP['id'], 'weixin_openid'=>$weixin_openid));
-    }else if(0 == $status){
+    }else if(null != $isgroup['isgroup']){
         $item = mysqld_select("SELECT praise FROM " . table('group') . " WHERE id = :id", array(':id'=>$_GP['id']));
         $item['praise']++;
         mysqld_update('group', array('praise' => $item['praise']), array('id'=>$_GP['id']));
-        $status = 1;
+        $isgroup['isgroup'] = 1;
         mysqld_update('group_user', array('isgroup' => 1), array('goodid'=>$_GP['id'], 'weixin_openid'=>$weixin_openid));
     }else{
         $item = mysqld_select("SELECT praise FROM " . table('group') . " WHERE id = :id", array(':id'=>$_GP['id']));
         $item['praise']++;
         mysqld_update('group', array('praise' => $item['praise']), array('id'=>$_GP['id']));
-        $status = 1;
+        $isgroup['isgroup'] = 1;
         mysqld_insert('group_user', array('isgroup' => 1, 'goodid'=>$_GP['id'], 'weixin_openid'=>$weixin_openid, 'openid'=>$openid));
     }
-    die(json_encode(array("result" => 1, "total" => $item['praise'], 'status' => $status)));
+    die(json_encode(array("result" => 1, "total" => $item['praise'], 'statue' => $isgroup['isgroup'])));
 }
